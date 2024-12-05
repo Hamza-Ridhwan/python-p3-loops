a58273b1a4ca9efd09fc33ee25c256b981af80c6
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-12-05 19:39:16 +0300

    Completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    year = 10
+    while year > 0:
+        print(year)
+        year+= -1
+    print('Happy New Year!') 
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [i**2 for i in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for numbers in range(100):
+        new_num = numbers + 1
+        if new_num % 3 == 0 and new_num % 5 == 0:
+            print('FizzBuzz')
+        elif new_num % 3 == 0:
+            print('Fizz')
+        elif new_num % 5 == 0:
+            print ('Buzz')
+        else:
+            print(new_num)
+
+
+fizzbuzz()
